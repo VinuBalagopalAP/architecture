83f2fa59fc52e1fb0d99e8bf51af94227d67ba4e
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-01-27 13:53:20 +0530

    color style added

--- lib/ui_architecture/color/color.dart
+++ lib/ui_architecture/color/color.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+const Color kcPrimaryColor = Color(0xff22AF5D);
+const Color kcMediumGreyColor = Color(0xff868686);
+const Color kcAccentColor = Color(0xffe5e5e5);
+const Color kcErrorColor = Color(0xff2f2f2f);
