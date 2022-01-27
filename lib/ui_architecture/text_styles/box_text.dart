90722aac4a4608f0d39714f963fc385bff94fb21
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-01-27 13:46:45 +0530

    text styles added

---
+++ lib/ui_architecture/text_styles/box_text.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:architecture/ui_architecture/text_styles/styles.dart';
+import 'package:flutter/material.dart';
+
+class BoxText extends StatelessWidget {
+  const BoxText({
+    Key? key,
+    required this.text,
+    this.style,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle? style;
+
+  const BoxText.headingOne(this.text) : style = heading1Style;
+  const BoxText.headingTwo(this.text) : style = heading2Style;
+  const BoxText.headingThree(this.text) : style = heading3Style;
+  const BoxText.headline(this.text) : style = headlineStyle;
+  const BoxText.subheading(this.text) : style = subheadingStyle;
+  const BoxText.caption(this.text) : style = captionStyle;
+
+  BoxText.body(this.text, {Color color = kcMediumGreyColor})
+      : style = bodyStyle.copyWith(color: color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style,
+    );
+  }
+}
